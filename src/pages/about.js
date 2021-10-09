1cf5a8270c0f66c09e2c6363018f2887a56019ec
Roberto Zaniboni <zaniboni@gmail.com>
2021-10-09 09:57:35 +0200

    Add and use a shared layout component

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout.js';
+
+export default function AboutPage() {
+  return (
+    <Layout
+      title="About This Site"
+      description="More information about this site."
+    >
+      <h1>About This Site</h1>
+      <Link to="/">Go home</Link>
+    </Layout>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout.js';
+
+export default function IndexPage() {
+  return (
+    <Layout>
+      <h1>Hello Frontend Masters</h1>
+      <Link to="/about">About this site</Link>
+    </Layout>
+  );
+}
